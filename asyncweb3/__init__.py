01af0260d7a1fe8241de1298e19a29af16f4c1e7
TennisBowling <33209196+TennisBowling@users.noreply.github.com>
2021-10-06 09:12:08 -0700

    Update __init__.py

--- asyncweb3/__init__.py
+++ asyncweb3/__init__.py
@@ -1,4 +1,3 @@
-import asyncio
 import aiohttp
 
 
@@ -6,15 +5,12 @@ class Asyncweb3:
     def __init__(self, provider: str, *, session: aiohttp.ClientSession=None):
         self.provider = provider
         
-        async def create_session():
-            self.session = session or aiohttp.ClientSession()
-
-        asyncio.get_event_loop().run_until_complete(create_session())
+        self.session = session or aiohttp.ClientSession()
     
-    def __aenter__(self):
+    async def __aenter__(self):
         return self
     
-    def __aexit__(self, *args):
+    async def __aexit__(self, *args):
         pass
     
     async def net_version(self):
@@ -22,7 +18,7 @@ class Asyncweb3:
         async with self.session.post(self.provider, json={'jsonrpc': '2.0', 'method': 'net_version', 'params': [], 'id': 1}, headers={'Content-type': 'application/json'}) as resp:
             return (await resp.json())['result']
 
-def asyncweb3(provider: str, *, session: aiohttp.ClientSession=None):
+def asyncweb3(provider: str, *, session: aiohttp.ClientSession=None) -> Asyncweb3:
     """The recommended way to start using asyncweb3. It takes a ``provider`` which should be a json-rpc http/https url, as well as an optional ``session`` kwarg that needs to be an aiohttp.ClientSession.\n
     It's recommeded to use it as such:\n
     .. code-block:: python
@@ -30,3 +26,4 @@ def asyncweb3(provider: str, *, session: aiohttp.ClientSession=None):
       async with asyncweb3.asyncweb3('http://localhost:8545') as w3:
           w3.function()
     """
+    return Asyncweb3(provider, session=session)
